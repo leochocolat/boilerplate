cf69e7f185eaf7bb9bfed9e6a743ec91835971a6
leomouraire1 <leomouraire1@gmail.com>
2019-05-26 00:16:28 +0200

    import object example

---
+++ src/javascript/components/Title.js
@@ -0,0 +1,27 @@
+//IMPORTS
+
+
+class Title {
+
+  constructor() {
+
+  }
+
+  createTitle(content) {
+
+    let div = document.createElement("div");
+    div.innerHTML = content;
+    div.style.position = "absolute";
+    div.style.left = "50%";
+    div.style.top = "50%";
+    div.style.transform = "translate(-50%, -50%)"
+    console.log(div);
+    document.body.appendChild(div);
+
+  }
+
+
+}
+
+
+export default new Title();
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,11 +1,4 @@
-import _ from 'lodash';
+//IMPORTS
+import Title from './components/Title.js';
 
-function component() {
-  const element = document.createElement('div');
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
+Title.createTitle("Boilerplate by Léo Mouraire");
