6d8a852eec87bd1210e0e83bb4e10def1df27b16
leomouraire1 <leomouraire1@gmail.com>
2019-05-26 00:34:37 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 const path = require('path');
 
 module.exports = {
+  mode: 'development',
   entry: './src/javascript/index.js',
+  watch: true,
   output: {
     filename: 'bundle.js',
     path: path.resolve('dist', 'javascript')
