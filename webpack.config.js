bb8e2849e4d59cc89ef4089af537582335b46770
leomouraire1 <leomouraire1@gmail.com>
2019-07-10 21:06:54 +0200

    asset managment

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,31 @@ module.exports = {
             presets: ['@babel/preset-env']
           }
         }
-      }
+      },
+      {
+          test: /\.(png|svg|jpg|gif)$/,
+          use: [
+            'file-loader'
+          ]
+       },
+       {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          use: [
+            'file-loader'
+          ]
+       },
+       {
+          test: /\.(csv|tsv)$/,
+          use: [
+            'csv-loader'
+          ]
+       },
+       {
+          test: /\.xml$/,
+          use: [
+            'xml-loader'
+          ]
+       }
     ]
   }
 };
