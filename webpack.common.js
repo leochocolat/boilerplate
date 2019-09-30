45208a551dbbdf12179cd760044d6b83af8e9394
leomouraire1 <leomouraire1@gmail.com>
2019-09-30 12:32:43 +0200

    add copy plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,31 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const copyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: {
-    app: './src/javascript/index.js'
+    app: "./src/javascript/index.js"
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Production'
-    })
+      title: "Production"
+    }),
+    new copyPlugin([
+      {
+        from: "./src/index.html",
+        to: path.resolve(__dirname, "dist")
+      },
+      {
+        from: "src/assets/",
+        to: path.resolve(__dirname, "dist", "assets")
+      }
+    ])
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve('dist', 'javascript')
+    filename: "[name].bundle.js",
+    path: path.resolve("dist", "javascript")
   },
   module: {
     rules: [
@@ -23,36 +33,28 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
-       },
-       {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader'
-          ]
-       },
-       {
-          test: /\.(csv|tsv)$/,
-          use: [
-            'csv-loader'
-          ]
-       },
-       {
-          test: /\.xml$/,
-          use: [
-            'xml-loader'  
-          ]
-       }
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      }
     ]
   }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,32 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const copyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    app: './src/javascript/index.js',
+    app: "./src/javascript/index.js"
   },
   plugins: [
-      new CleanWebpackPlugin()
-   ],
+    new CleanWebpackPlugin(),
+    new copyPlugin([
+      {
+        from: "./src/index.html",
+        to: path.resolve(__dirname, "dist")
+      },
+      {
+        from: "src/assets/",
+        to: path.resolve(__dirname, "dist", "assets")
+      }
+    ])
+  ],
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist"
   },
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve('dist', 'javascript')
+    filename: "[name].bundle.js",
+    path: path.resolve("dist", "javascript")
   },
   module: {
     rules: [
@@ -24,36 +34,28 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /(node_modules|bower_components)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env']
+            presets: ["@babel/preset-env"]
           }
         }
       },
       {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
-       },
-       {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader'
-          ]
-       },
-       {
-          test: /\.(csv|tsv)$/,
-          use: [
-            'csv-loader'
-          ]
-       },
-       {
-          test: /\.xml$/,
-          use: [
-            'xml-loader'  
-          ]
-       }
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      }
     ]
   }
 };
